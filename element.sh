58136dbaa881cebe28f70c1b5cb976afc29880b9
amjadsh92 <amjadsharafeddien@gmail.com>
2025-05-12 18:42:33 +0300

    feat: search for name when giving an integer argument

--- element.sh
+++ element.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
-
+PSQL="psql --username=amjad --dbname=periodic_table -t --no-align -c"
 if [[ -z $1 ]]
 then
-echo -e "Please provide an element as an argument."
+echo "Please provide an element as an argument."
+elif [[ $1 =~ ^[0-9]+$ ]]
+then
+name=$($PSQL "SELECT name FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")
+echo $name
 fi

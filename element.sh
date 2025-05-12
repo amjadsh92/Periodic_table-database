c5ce9cb95839532f9e80951aa86fad0a85805a36
amjadsh92 <amjadsharafeddien@gmail.com>
2025-05-12 18:44:54 +0300

    fix: write name variable in capital letter

--- element.sh
+++ element.sh
@@ -5,6 +5,6 @@ then
 echo "Please provide an element as an argument."
 elif [[ $1 =~ ^[0-9]+$ ]]
 then
-name=$($PSQL "SELECT name FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")
-echo $name
+NAME=$($PSQL "SELECT name FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")
+
 fi

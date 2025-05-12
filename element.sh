9a9af8bad9ccaf3bcdbb752cd8b05caf6f55560d
amjadsh92 <amjadsharafeddien@gmail.com>
2025-05-12 19:09:45 +0300

    feat: search for melting and boiling point when giving an integer argument

--- element.sh
+++ element.sh
@@ -9,4 +9,7 @@ NAME=$($PSQL "SELECT name FROM elements INNER JOIN properties USING(atomic_numbe
 SYMBOL=$($PSQL "SELECT symbol FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")
 TYPE=$($PSQL "SELECT type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")
 ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")
+MELTING_POINT_CELSIUS=$($PSQL "SELECT melting_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")
+BOILING_POINT_CELSIUS=$($PSQL "SELECT boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")
+$BOILING_POINT_CELSIUS
 fi
